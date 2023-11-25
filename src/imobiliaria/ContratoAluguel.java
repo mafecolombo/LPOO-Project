d99b865d2a3aac7c5b8da67184ae786613e552b1
Maria Fernanda Colombo <140662320+mafecolombo@users.noreply.github.com>
2023-11-25 08:50:01 -0400

    Updating ContratoAluguel

---
+++ src/imobiliaria/ContratoAluguel.java
@@ -0,0 +1,9 @@
+package imobiliaria;
+
+public class ContratoAluguel<T> {
+    private Imovel imovel;
+    private Corretor corretor;
+    private Cliente cliente;
+    private T tempo;
+    private double valor;
+}
