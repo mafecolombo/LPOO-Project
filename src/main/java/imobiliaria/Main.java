5657ca320601cecfefd59b0b84652e9e1246a82f
Maria Fernanda Colombo <140662320+mafecolombo@users.noreply.github.com>
2023-11-25 15:08:24 -0400

    a

--- src/main/java/imobiliaria/Main.java
+++ src/main/java/imobiliaria/Main.java
@@ -1,24 +1,97 @@
 package imobiliaria;
 
+import java.util.*;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
+//import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+//import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void cadastrarEndereco(List<Endereco> enderecos, Stage primaryStage){
+
+        Text RuaText = new Text("Rua: ");
+        RuaText.getStyleClass().add("text");
+        TextField ruaInput = new TextField();
+
+        Text NumeroText = new Text("Numero: ");
+        NumeroText.getStyleClass().add("text");
+        TextField numeroInput = new TextField();
+
+        Text BairroText = new Text("Bairro: ");
+        BairroText.getStyleClass().add("text");
+        TextField bairroInput = new TextField();
+
+        Text CidadeText = new Text("Cidade: ");
+        CidadeText.getStyleClass().add("text");
+        TextField cidadeInput = new TextField();
+
+        Text CEPText = new Text("CEP: ");
+        CEPText.getStyleClass().add("text");
+        TextField CEPInput = new TextField();
+
+        Button submitButton = new Button("Cadastrar");
+        submitButton.getStyleClass().add("button");
+
+        submitButton.setOnAction(event -> {
+            String rua = ruaInput.getText();
+
+            String num = numeroInput.getText();
+            int numero = Integer.parseInt(num);
+
+            String bairro = bairroInput.getText();
+
+            String cidade = cidadeInput.getText();
+
+            String CEP = CEPInput.getText();
+
+            Endereco endereco = new Endereco(rua, numero, bairro, cidade, CEP);
+            enderecos.add(endereco);
+
+            start(primaryStage);
+            
+        });
+
+
+        VBox cadastrar = new VBox(10);
+        cadastrar.setAlignment(Pos.CENTER);
+        cadastrar.getChildren().addAll(
+                RuaText,
+                ruaInput,
+                NumeroText,
+                numeroInput,
+                BairroText,
+                bairroInput,
+                CidadeText,
+                cidadeInput,
+                CEPText,
+                CEPInput,
+                submitButton
+        );
+
+        Scene scene = new Scene(cadastrar, 1000, 500);
+        scene.getStylesheets().add(getClass().getResource("/estilo.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
     
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Controle de Imóveis");
+        List<Endereco> enderecos = new ArrayList<Endereco>();
 
         Text tittleMenu = new Text("Bem-vindo ao Sistema de Controle de Imóveis");
         Text space01 = new Text("");
@@ -91,6 +164,10 @@ public class Main extends Application {
         primaryStage.show();
 
         // Adicione a lógica de eventos dos botões conforme necessário
+
+        cadastrarClientesButton.setOnAction(event -> {
+            cadastrarEndereco(enderecos, primaryStage);
+        });
     }
 
 }
